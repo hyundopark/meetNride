282aa9ef746d48eedba029a106fc845f2b305b68
Hyun Do Park <hyundopark@Hyun-Dos-MacBook-Pro.local>
2018-05-27 14:25:11 -0700

    morning commit

--- content/src/components/CreateGroup.js
+++ content/src/components/CreateGroup.js
@@ -10,6 +10,7 @@ import {
   Container
 } from "reactstrap";
 import "./css/createGroup.css";
+import TestSelect from "./TestSelect";
 
 class CreateGroup extends React.Component {
   constructor(props) {
@@ -17,10 +18,15 @@ class CreateGroup extends React.Component {
     this.state = {};
 
     this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
+    this.onSave = this.onSave.bind(this);
   }
 
   onRadioBtnClick(selected) {
     this.setState({ selected });
+  }
+
+  onSave() {
+    console.log("testing");
   }
 
   render() {
@@ -29,7 +35,7 @@ class CreateGroup extends React.Component {
         <div className="div_createGroup">
           <Form className="form_createGroup">
             <h5 id="createGroup">Create a group</h5>
-            <FormGroup class="col-md-6">
+            <FormGroup>
               <Label for="groupName">Group Name</Label>
               <Input
                 type="text"
@@ -38,21 +44,9 @@ class CreateGroup extends React.Component {
                 placeholder="Enter Your Group Name"
               />
             </FormGroup>
-            <FormGroup>
-              <Label for="exampleSelectMulti">Select Multiple</Label>
-              <Input
-                type="select"
-                name="selectMulti"
-                id="exampleSelectMulti"
-                multiple
-              >
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </Input>
-            </FormGroup>
+            <Label for="rideSelect">Key Words</Label>
+            <TestSelect id="rideSelect" />
+            <br />
             <FormGroup>
               <Label for="groupDescribe">Group Description</Label>
               <Input type="textarea" name="groupDescribe" id="groupDescribe" />
@@ -60,7 +54,7 @@ class CreateGroup extends React.Component {
             <br />
             <br />
             <div>
-              <Button outline color="primary">
+              <Button outline color="primary" onClick={this.onSave}>
                 Submit
               </Button>{" "}
               <Button outline color="secondary">
