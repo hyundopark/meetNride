ab22fa450c90e54238b350676c5d89b39960c482
Hyun Do Park <hyundopark@Hyun-Dos-MacBook-Pro.local>
2018-08-21 12:41:11 -0700

    morning commit before getting started

--- content/src/components/CreateGroup.js
+++ content/src/components/CreateGroup.js
@@ -1,37 +1,61 @@
 import React from "react";
-import {
-  Button,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  FormText,
-  ButtonGroup,
-  Container
-} from "reactstrap";
+import { Button, Form, FormGroup, Label, Input, Container } from "reactstrap";
 import "./css/createGroup.css";
 import TestSelect from "./TestSelect";
 
 class CreateGroup extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+
+    const formData = {
+      ridingGroupName: "",
+      groupKeyWord: "",
+      groupDescribe: ""
+    }
+    this.state = {
+      formData: formData
+      // ridingGroupName: "",
+      // groupKeyWord: "",
+      // groupDescribe: ""
+    };
 
     this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
     this.onSave = this.onSave.bind(this);
     this.onChange = this.onChange.bind(this);
   }
 
-  onRadioBtnClick(selected) {
-    this.setState({ selected });
-  }
-
-  onSave() {
-    console.log("testing");
+  onRadioBtnClick(selectedKeyWord) {
+    this.setState(prevState => {
+      return { ...prevState, groupKeyWord: selectedKeyWord };
+    });
   }
 
   onChange(e){
-    console.log(e)
+    const target = e.target;
+    const value = target.value;//if there's radio, also have it target.checked.
+    const name = target.name;
+    this.setState(prevState=>{
+
+      const field = {...prevState.formData[name]}
+
+      field.value = value;
+      const formData = {...prevState.formData, [name]: field}
+
+      return {formData: formData}
+    })
+
+  }
+  // onChange(e){
+  //   const target = e.target;
+  //   this.setState(prevState=>{
+  //     return {...prevState, groupDescribe: target.value}
+
+  //   })
+
+  // }
+
+  onSave() {
+    console.log("testing");
   }
 
   render() {
@@ -41,21 +65,30 @@ class CreateGroup extends React.Component {
           <Form className="form_createGroup">
             <h5 id="createGroup">Create a group</h5>
             <FormGroup>
-              <Label for="groupName">Group Name</Label>
+              <Label for="ridingGroupName">Group Name</Label>
               <Input
-                onChange={(e) => this.onChange(`${e.target.value}`)}
+                onChange={e => this.onChange(e)}
                 type="text"
-                name="groupName"
-                id="groupName"
+                name="ridingGroupName"
+                id="ridingGroupName"
                 placeholder="Enter Your Group Name"
               />
             </FormGroup>
             <Label for="rideSelect">Key Words</Label>
-            <TestSelect id="rideSelect" sendDataKeyWordSelect={this.onRadioBtnClick}/>
+            <TestSelect
+              id="rideSelect"
+              sendDataKeyWordSelect={this.onRadioBtnClick}
+            />
             <br />
             <FormGroup>
               <Label for="groupDescribe">Group Description</Label>
-              <Input type="textarea" name="groupDescribe" id="groupDescribe" />
+              <Input
+                type="textarea"
+                name="groupDescribe"
+                id="groupDescribe"
+                onChange={this.onChange}
+                value={this.state.groupDescribe}
+              />
             </FormGroup>
             <br />
             <div>
